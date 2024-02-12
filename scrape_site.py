30b64e238696e455c8b354d35dd8e63593eaddd9
Erick Ramirez <erickramirezau@apache.org>
2024-02-12 07:12:36 +0000

    Modified bot to only respond to at-mentions, added scrape_site.py

---
+++ scrape_site.py
@@ -0,0 +1,18 @@
+import os
+
+from urllib.parse import urlparse
+
+from dotenv import load_dotenv
+
+from chatbot_api.crawl_scrape_docs import crawl_website_parallel
+from pipeline.config import load_config
+
+load_dotenv(".env")
+config = load_config("config.yml")
+
+# Astra docs
+for website in config.doc_pages:
+    parsed_website = urlparse(website)
+    basename_website = os.path.basename(parsed_website.path)
+
+    crawl_website_parallel(website, os.path.join("output", f"{basename_website}.txt"), recursive=False)
